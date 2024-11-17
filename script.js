4325d2060ece48f6a607788ae51a55a7913bcef1
SIVARAMAKRISHNAN T <strsivaram486@gmail.com>
2024-11-17 17:36:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Your message has been sent!");
+  // Here you can implement real email functionality or form submission via an API
+});
